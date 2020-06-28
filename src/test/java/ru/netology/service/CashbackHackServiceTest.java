4643bf06c4010946034c6976655b5f100c626dde
Alexander Pavlenko <pava@bk.ru>
2020-06-28 11:46:58 +0700

    Code review

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,21 +6,21 @@ public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
     @org.testng.annotations.Test
-    public void shoulRemainByAmount999() {
+    public void shouldRemainByAmount999() {
         int amount = 999;
         int expected = 1;
         assertEquals(service.remain(amount), expected);
     }
 
     @org.testng.annotations.Test
-    public void shoulRemainByAmount1000() {
+    public void shouldRemainByAmount1000() {
         int amount = 1000;
         int expected = 0;
         assertEquals(service.remain(amount), expected);
     }
 
     @org.testng.annotations.Test
-    public void shoulRemainByAmount1001() {
+    public void shouldRemainByAmount1001() {
         int amount = 1001;
         int expected = 999;
         assertEquals(service.remain(amount), expected);
