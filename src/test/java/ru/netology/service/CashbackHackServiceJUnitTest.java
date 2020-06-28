4d3247f278aa5f4677e90cda35e75d3a3413c679
Alexander Pavlenko <pava@bk.ru>
2020-06-28 11:42:34 +0700

    Code review

--- src/test/java/ru/netology/service/CashbackHackServiceJUnitTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnitTest.java
@@ -8,21 +8,21 @@ public class CashbackHackServiceJUnitTest {
     private CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shoulRemainByAmount999() {
+    public void shouldRemainByAmount999() {
         int amount = 999;
         int expected = 1;
         assertEquals(expected, service.remain(amount));
     }
 
     @Test
-    public void shoulRemainByAmount1000() {
+    public void shouldRemainByAmount1000() {
         int amount = 1000;
         int expected = 0;
         assertEquals(expected, service.remain(amount));
     }
 
     @Test
-    public void shoulRemainByAmount1001() {
+    public void shouldRemainByAmount1001() {
         int amount = 1001;
         int expected = 999;
         assertEquals(expected, service.remain(amount));
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,21 +6,21 @@ public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
     @org.junit.Test
-    public void shoulRemainByAmount999() {
+    public void shouldRemainByAmount999() {
         int amount = 999;
         int expected = 1;
         assertEquals(expected, service.remain(amount));
     }
 
     @org.junit.Test
-    public void shoulRemainByAmount1000() {
+    public void shouldRemainByAmount1000() {
         int amount = 1000;
         int expected = 0;
         assertEquals(expected, service.remain(amount));
     }
 
     @org.junit.Test
-    public void shoulRemainByAmount1001() {
+    public void shouldRemainByAmount1001() {
         int amount = 1001;
         int expected = 999;
         assertEquals(expected, service.remain(amount));
