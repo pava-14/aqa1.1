049d628e323bc41f12c8d85aa19c5994470e3317
Alexander Pavlenko <pava@bk.ru>
2020-06-29 15:50:03 +0700

    Teacher's comment #1

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 public class CashbackHackService {
     private final int boundary = 1000;
+
     public int remain(int amount) {
         return boundary - amount % boundary;
     }
